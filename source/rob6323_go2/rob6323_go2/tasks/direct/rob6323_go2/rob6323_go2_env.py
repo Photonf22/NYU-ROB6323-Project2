6232a9d00a8e606aa54421b86d990ee0bb4cec26
Alejandro Sanchez <Sanchez.alef22@gmail.com>
2025-12-15 21:18:48 -0500

    added part 5, and now will test on hpc

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
@@ -34,7 +34,10 @@ class Rob6323Go2Env(DirectRLEnv):
         self._previous_actions = torch.zeros(
             self.num_envs, gym.spaces.flatdim(self.single_action_space), device=self.device
         )
-
+        self.rew_orient= torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
+        self.rew_lin_vel_z= torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
+        self.rew_dof_vel= torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
+        self.rew_ang_vel_xy = torch.zeros(self.num_envs, dtype=torch.float, device=self.device, requires_grad=False)
         # X/Y linear velocity and yaw angular velocity commands
         self._commands = torch.zeros(self.num_envs, 3, device=self.device)
         # Get specific body indices
@@ -51,10 +54,10 @@ class Rob6323Go2Env(DirectRLEnv):
                 "track_ang_vel_z_exp",
                 "rew_action_rate",
                 "raibert_heuristic",
-                #"orient",
-                #"lin_vel_z",
-                #"dof_vel",
-                #"ang_vel_xy",
+                "orient",
+                "lin_vel_z",
+                "dof_vel",
+                "ang_vel_xy",
                 #"feet_clearance",
                 #"tracking_contacts_shaped_force",
             ]
@@ -172,13 +175,32 @@ class Rob6323Go2Env(DirectRLEnv):
         # yaw rate tracking
         yaw_rate_error = torch.square(self._commands[:, 2] - self.robot.data.root_ang_vel_b[:, 2])
         yaw_rate_error_mapped = torch.exp(-yaw_rate_error / 0.25)
-        
+        # 1. Penalize non-vertical orientation (projected gravity on XY plane)
+        # Hint: We want the robot to stay upright, so gravity should only project onto Z.
+        # Calculate the sum of squares of the X and Y components of projected_gravity_b.
+        self.rew_orient = torch.sum(torch.square(self.robot.data.projected_gravity_b[:,0]), dim=1) + torch.sum(torch.square(self.robot.data.projected_gravity_b[:,1]), dim=1)
+
+        # 2. Penalize vertical velocity (z-component of base linear velocity)
+        # Hint: Square the Z component of the base linear velocity.
+        self.rew_lin_vel_z = self.robot.data.root_lin_vel_b[:, 2]**2
+
+        # 3. Penalize high joint velocities
+        # Hint: Sum the squares of all joint velocities.
+        self.rew_dof_vel = torch.sum(torch.square(self.robot.data.joint_vel), dim=1)
+
+        # 4. Penalize angular velocity in XY plane (roll/pitch)
+        # Hint: Sum the squares of the X and Y components of the base angular velocity.
+        self.rew_ang_vel_xy = torch.sum(torch.square(self.robot.data.root_ang_vel_b[:,0]),dim=1) + torch.sum(torch.square(self.robot.data.root_ang_vel_b[:,1]),dim=1) 
         rewards = {
             "track_lin_vel_xy_exp": lin_vel_error_mapped * self.cfg.lin_vel_reward_scale * self.step_dt,
             "track_ang_vel_z_exp": yaw_rate_error_mapped * self.cfg.yaw_rate_reward_scale * self.step_dt,
             "rew_action_rate": rew_action_rate * self.cfg.action_rate_reward_scale,
             # Note: This reward is negative (penalty) in the config
             "raibert_heuristic": rew_raibert_heuristic * self.cfg.raibert_heuristic_reward_scale, 
+            "orient": self.rew_orient * self.cfg.orient_reward_scale,
+            "lin_vel_z": self.rew_lin_vel_z * self.cfg.lin_vel_z_reward_scale,
+            "dof_vel": self.rew_dof_vel * self.cfg.dof_vel_reward_scale,
+            "ang_vel_xy": self.rew_ang_vel_xy * self.cfg.ang_vel_xy_reward_scale,
         }
         reward = torch.sum(torch.stack(list(rewards.values())), dim=0)
         # Logging
